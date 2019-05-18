4edd709b737a17ebe81af7125514ef645a0d942b
bettyChen <s9654025@gmail.com>
2019-05-18 15:23:55 +0800

    Updates

---
+++ precache-manifest.bbc047237ea4dfc77842d22db6502534.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11b969170ec695a1fbeddffa21d7db60",
+    "url": "/wowsushi/Tic-Tac-Toe.git/index.html"
+  },
+  {
+    "revision": "bc9f10bf6cc5deb635d6",
+    "url": "/wowsushi/Tic-Tac-Toe.git/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/wowsushi/Tic-Tac-Toe.git/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "bc9f10bf6cc5deb635d6",
+    "url": "/wowsushi/Tic-Tac-Toe.git/static/js/main.9134e1f8.chunk.js"
+  },
+  {
+    "revision": "3e4fb4367226daed5c11",
+    "url": "/wowsushi/Tic-Tac-Toe.git/static/js/runtime~main.f8a89e9c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wowsushi/Tic-Tac-Toe.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
