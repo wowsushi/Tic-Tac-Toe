9991cef59c537806d6962f3649f21264f5ad3746
bettyChen <s9654025@gmail.com>
2019-05-18 18:46:02 +0800

    Updates

---
+++ precache-manifest.20cb19eacbea05a714c925c350f36005.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46975751438a0cac52a7c1ef15786db9",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "f73958d4c63eaf907bd8",
+    "url": "/Tic-Tac-Toe/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/Tic-Tac-Toe/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "f73958d4c63eaf907bd8",
+    "url": "/Tic-Tac-Toe/static/js/main.377f753e.chunk.js"
+  },
+  {
+    "revision": "10779627d94960e7b271",
+    "url": "/Tic-Tac-Toe/static/js/runtime~main.adeb2bb3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Tic-Tac-Toe/static/media/logo.5d5d9eef.svg"
+  }
+]);
